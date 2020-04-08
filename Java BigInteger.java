4fbe96b715861696c0478fa47f0677a907192b2a
nurulmadarina <nurul.lailani.m@mail.ugm.ac.id>
2020-04-08 19:40:37 +0700

    add solution Java bigInteger

---
+++ Java BigInteger.java
@@ -0,0 +1,13 @@
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BigInteger bi = sc.nextBigInteger();
+        BigInteger bu = sc.nextBigInteger();
+        BigInteger a, b;
+        a = bi.add(bu);
+        b = bi.multiply(bu)
+        System.out.println (a);
+        System.out.println (b);
+        }
+}
